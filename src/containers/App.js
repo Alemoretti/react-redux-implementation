8443b937414b4711ca7e81dd29945cc2d61e7598
alemoretti <alemoretti2010@hotmail.com>
2020-05-31 17:31:12 -0300

    Insert lifecycle methods.

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: 'asfa1', name: 'Max', age: 28 },
@@ -12,6 +17,11 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps (props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
   }
 
   nameChangedHandler = ( event, id ) => {
@@ -45,7 +55,16 @@ class App extends Component {
     this.setState( { showPersons: !doesShow } );
   }
 
+  //componentWillMount() {
+  //  console.log('componentWillMount');
+  //}
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
   render () {
+    console.log('[App.js] render');
     let persons = null;
 
     if ( this.state.showPersons ) {
