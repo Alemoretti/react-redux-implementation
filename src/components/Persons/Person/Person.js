0fc2f5a7b3c276f94cd14d5bf72c1804382f0fd3
alemoretti <alemoretti2010@hotmail.com>
2020-06-05 02:11:46 -0300

    Use React createRef to focus last name input.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,13 +4,26 @@ import classes from './Person.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render () {
         console.log('[Person.js] rendering...');
         return (
             <Auxiliary>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
+                <p key="i2">{this.props.children}</p>
                 <input 
+                    key="i3"
+                    //ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name} 
