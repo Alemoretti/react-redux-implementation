7bf3d509bb091b13b3d3611d22b9c42bfb6e29d8
alemoretti <alemoretti2010@hotmail.com>
2020-06-05 02:27:55 -0300

    Add ref to function component, to click when component is rendered.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = props => {
+    const toggleBtnRef = useRef(null);
+
     useEffect(() => {
       console.log('[Cockpit.js] useEffect updated');
       setTimeout(() => {
         //alert('Saved data to cloud!');
       }, 1000);
+      toggleBtnRef.current.click();
       return () => {
         console.log('[Cockpit.js] cleanup work in useEffect')
       }
@@ -36,7 +39,7 @@ const cockpit = props => {
         <div className={classes.Cockpit}>
             <h1>Hi, I'm a React App</h1>
             <p className={asssignedClasses.join(' ')}>This is really working!</p>
-            <button className={btnClass} onClick={props.clicked}>
+            <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
             Toggle Persons
             </button>        
         </div>
